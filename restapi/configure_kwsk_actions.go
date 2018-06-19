4a33af328495d514732433a83d138f65fc748a6d
Ben Browning <bbrownin@redhat.com>
2018-06-19 11:57:37 -0400

    Store actions as Configurations in Knative
    This does some basic wiring up of `wsk action create` and `wsk action
    list` to use Knative CRDs. You can create an action and subsequently
    list it. Only the action's name and namespace are populated at the
    moment.
    
    We're not yet doing anything with the action's code nor creating any
    kind of Knative build objects for the action.

--- restapi/configure_kwsk_actions.go
+++ restapi/configure_kwsk_actions.go
@@ -5,11 +5,14 @@ import (
 
 	middleware "github.com/go-openapi/runtime/middleware"
 
+	models "github.com/projectodd/kwsk/models"
 	"github.com/projectodd/kwsk/restapi/operations"
 	"github.com/projectodd/kwsk/restapi/operations/actions"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/knative/serving/pkg/apis/serving/v1alpha1"
 	knative "github.com/knative/serving/pkg/client/clientset/versioned"
 )
 
@@ -22,27 +25,75 @@ func configureActions(api *operations.KwskAPI, knativeClient *knative.Clientset)
 		return actions.NewGetActionByNameOK()
 	})
 
-	api.ActionsGetAllActionsHandler = actions.GetAllActionsHandlerFunc(actionsGetAllActionsFunc(knativeClient))
+	api.ActionsGetAllActionsHandler = actions.GetAllActionsHandlerFunc(getAllActionsFunc(knativeClient))
 
 	api.ActionsInvokeActionHandler = actions.InvokeActionHandlerFunc(func(params actions.InvokeActionParams) middleware.Responder {
 		return middleware.NotImplemented("operation actions.InvokeAction has not yet been implemented")
 	})
 
-	api.ActionsUpdateActionHandler = actions.UpdateActionHandlerFunc(func(params actions.UpdateActionParams) middleware.Responder {
-		return actions.NewUpdateActionOK()
-	})
+	api.ActionsUpdateActionHandler = actions.UpdateActionHandlerFunc(updateActionFunc(knativeClient))
 }
 
-func actionsGetAllActionsFunc(knativeClient *knative.Clientset) actions.GetAllActionsHandlerFunc {
+func updateActionFunc(knativeClient *knative.Clientset) actions.UpdateActionHandlerFunc {
+	return func(params actions.UpdateActionParams) middleware.Responder {
+		namespace := namespaceOrDefault(params.Namespace)
+		container := corev1.Container{
+			Name: "action",
+		}
+		config := &v1alpha1.Configuration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      params.ActionName,
+				Namespace: namespace,
+			},
+			Spec: v1alpha1.ConfigurationSpec{
+				Generation: 0,
+				RevisionTemplate: v1alpha1.RevisionTemplateSpec{
+					Spec: v1alpha1.RevisionSpec{
+						Container: container,
+					},
+				},
+			},
+		}
+		fmt.Printf("Creating action %+v\n", config)
+		_, err := knativeClient.ServingV1alpha1().Configurations(namespace).Create(config)
+		if err != nil {
+			msg := err.Error()
+			errorMessage := &models.ErrorMessage{
+				Error: &msg,
+			}
+			return actions.NewUpdateActionInternalServerError().WithPayload(errorMessage)
+		}
+		return actions.NewUpdateActionOK()
+	}
+}
+
+func getAllActionsFunc(knativeClient *knative.Clientset) actions.GetAllActionsHandlerFunc {
 	return func(params actions.GetAllActionsParams) middleware.Responder {
-		// TODO: This is just stubbed in here to show an example of
-		// fetching knative CRDs. The namespace should definitely not
-		// be hardcoded, for example.
-		configs, err := knativeClient.ServingV1alpha1().Configurations("default").List(metav1.ListOptions{})
+		namespace := namespaceOrDefault(params.Namespace)
+		configs, err := knativeClient.ServingV1alpha1().Configurations(namespace).List(metav1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("There are %d configs in the cluster\n", len(configs.Items))
-		return actions.NewGetAllActionsOK()
+		var payload = make([]*models.EntityBrief, len(configs.Items))
+		for i, item := range configs.Items {
+			name := item.ObjectMeta.Name
+			namespace := item.ObjectMeta.Namespace
+			payload[i] = &models.EntityBrief{
+				Name:      &name,
+				Namespace: &namespace,
+			}
+		}
+		return actions.NewGetAllActionsOK().WithPayload(payload)
 	}
+}
+
+func namespaceOrDefault(namespace string) string {
+	if namespace == "_" {
+		// TODO: In OpenWhisk land, the "_" namespace means the
+		// default namespace of the authenticated user. Because we're
+		// not dealing with any auth yet, just hardcode this to the
+		// "default" namespace for now.
+		namespace = "default"
+	}
+	return namespace
 }
