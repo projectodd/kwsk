b9a38f8cda49027b18258a4fad8b5f99fcc3bb36
Ben Browning <bbrownin@redhat.com>
2018-06-20 13:00:57 -0400

    Implement `wsk action delete` and more metadata for actions
    You still can't update existing actions, but we can at least create
    and delete actions now.
    
    We now store action kinds and versions as annotations on the
    Configuration object. Blackbox actions have their container image used
    in the RevisionTemplateSpec.

--- restapi/configure_kwsk_actions.go
+++ restapi/configure_kwsk_actions.go
@@ -10,6 +10,7 @@ import (
 	"github.com/projectodd/kwsk/restapi/operations/actions"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/knative/serving/pkg/apis/serving/v1alpha1"
@@ -17,13 +18,9 @@ import (
 )
 
 func configureActions(api *operations.KwskAPI, knativeClient *knative.Clientset) {
-	api.ActionsDeleteActionHandler = actions.DeleteActionHandlerFunc(func(params actions.DeleteActionParams) middleware.Responder {
-		return middleware.NotImplemented("operation actions.DeleteAction has not yet been implemented")
-	})
+	api.ActionsDeleteActionHandler = actions.DeleteActionHandlerFunc(deleteActionFunc(knativeClient))
 
-	api.ActionsGetActionByNameHandler = actions.GetActionByNameHandlerFunc(func(params actions.GetActionByNameParams) middleware.Responder {
-		return actions.NewGetActionByNameOK()
-	})
+	api.ActionsGetActionByNameHandler = actions.GetActionByNameHandlerFunc(getActionByNameFunc(knativeClient))
 
 	api.ActionsGetAllActionsHandler = actions.GetAllActionsHandlerFunc(getAllActionsFunc(knativeClient))
 
@@ -34,16 +31,40 @@ func configureActions(api *operations.KwskAPI, knativeClient *knative.Clientset)
 	api.ActionsUpdateActionHandler = actions.UpdateActionHandlerFunc(updateActionFunc(knativeClient))
 }
 
+func deleteActionFunc(knativeClient *knative.Clientset) actions.DeleteActionHandlerFunc {
+	return func(params actions.DeleteActionParams) middleware.Responder {
+		namespace := namespaceOrDefault(params.Namespace)
+		err := knativeClient.ServingV1alpha1().Configurations(namespace).Delete(params.ActionName, &metav1.DeleteOptions{})
+		if err != nil {
+			msg := err.Error()
+			errorMessage := &models.ErrorMessage{
+				Error: &msg,
+			}
+			if errors.IsNotFound(err) {
+				return actions.NewDeleteActionNotFound().WithPayload(errorMessage)
+			}
+			return actions.NewDeleteActionInternalServerError().WithPayload(errorMessage)
+		}
+		return actions.NewDeleteActionOK()
+	}
+}
+
 func updateActionFunc(knativeClient *knative.Clientset) actions.UpdateActionHandlerFunc {
 	return func(params actions.UpdateActionParams) middleware.Responder {
 		namespace := namespaceOrDefault(params.Namespace)
+
+		annotations := make(map[string]string)
+		annotations["kwsk_action_version"] = params.Action.Version
+		annotations["kwsk_action_kind"] = *params.Action.Exec.Kind
+
 		container := corev1.Container{
-			Name: "action",
+			Image: params.Action.Exec.Image,
 		}
 		config := &v1alpha1.Configuration{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      params.ActionName,
-				Namespace: namespace,
+				Name:        params.ActionName,
+				Namespace:   namespace,
+				Annotations: annotations,
 			},
 			Spec: v1alpha1.ConfigurationSpec{
 				Generation: 0,
@@ -55,7 +76,7 @@ func updateActionFunc(knativeClient *knative.Clientset) actions.UpdateActionHand
 			},
 		}
 		fmt.Printf("Creating action %+v\n", config)
-		_, err := knativeClient.ServingV1alpha1().Configurations(namespace).Create(config)
+		createdConfig, err := knativeClient.ServingV1alpha1().Configurations(namespace).Create(config)
 		if err != nil {
 			msg := err.Error()
 			errorMessage := &models.ErrorMessage{
@@ -63,7 +84,41 @@ func updateActionFunc(knativeClient *knative.Clientset) actions.UpdateActionHand
 			}
 			return actions.NewUpdateActionInternalServerError().WithPayload(errorMessage)
 		}
-		return actions.NewUpdateActionOK()
+		id := createdConfig.ObjectMeta.SelfLink
+		itemId := &models.ItemID{
+			ID: &id,
+		}
+		return actions.NewUpdateActionOK().WithPayload(itemId)
+	}
+}
+
+func getActionByNameFunc(knativeClient *knative.Clientset) actions.GetActionByNameHandlerFunc {
+	return func(params actions.GetActionByNameParams) middleware.Responder {
+		namespace := namespaceOrDefault(params.Namespace)
+		config, err := knativeClient.ServingV1alpha1().Configurations(namespace).Get(params.ActionName, metav1.GetOptions{})
+		if err != nil {
+			msg := err.Error()
+			errorMessage := &models.ErrorMessage{
+				Error: &msg,
+			}
+			if errors.IsNotFound(err) {
+				return actions.NewGetActionByNameNotFound().WithPayload(errorMessage)
+			}
+			return actions.NewGetActionByNameInternalServerError().WithPayload(errorMessage)
+		}
+		objectMeta := config.ObjectMeta
+		kind := objectMeta.Annotations["kwsk_action_kind"]
+		version := objectMeta.Annotations["kwsk_action_version"]
+		payload := &models.Action{
+			Name:      &objectMeta.Name,
+			Namespace: &objectMeta.Namespace,
+			Version:   &version,
+			Exec: &models.ActionExec{
+				Image: config.Spec.RevisionTemplate.Spec.Container.Image,
+				Kind:  &kind,
+			},
+		}
+		return actions.NewGetActionByNameOK().WithPayload(payload)
 	}
 }
 
@@ -72,15 +127,21 @@ func getAllActionsFunc(knativeClient *knative.Clientset) actions.GetAllActionsHa
 		namespace := namespaceOrDefault(params.Namespace)
 		configs, err := knativeClient.ServingV1alpha1().Configurations(namespace).List(metav1.ListOptions{})
 		if err != nil {
-			panic(err.Error())
+			msg := err.Error()
+			errorMessage := &models.ErrorMessage{
+				Error: &msg,
+			}
+			return actions.NewGetAllActionsInternalServerError().WithPayload(errorMessage)
 		}
 		var payload = make([]*models.EntityBrief, len(configs.Items))
 		for i, item := range configs.Items {
 			name := item.ObjectMeta.Name
 			namespace := item.ObjectMeta.Namespace
+			version := item.ObjectMeta.Annotations["kwsk_action_version"]
 			payload[i] = &models.EntityBrief{
 				Name:      &name,
 				Namespace: &namespace,
+				Version:   &version,
 			}
 		}
 		return actions.NewGetAllActionsOK().WithPayload(payload)
