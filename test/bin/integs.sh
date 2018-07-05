96e381b1c5f005e35c9e13e0c331341d89cb7c5a
Jim Crossley <jim@crossleys.org>
2018-07-05 10:57:11 -0400

    Fix killing of kwsk

--- test/bin/integs.sh
+++ test/bin/integs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -v
+set -x
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TESTDIR="$SCRIPTDIR/.."
@@ -12,15 +12,13 @@ if [ ! -d "$OWSK_HOME" ]; then
   git clone https://github.com/apache/incubator-openwhisk.git $OWSK_HOME
   sed -e "s:OPENWHISK_HOME:$OWSK_HOME:" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
 fi
-go run ${KWSK_HOME}cmd/kwsk-server/main.go --port 8080 --istio $(minikube ip):32000 >kwsk.log 2>&1 &
+setsid go run ${KWSK_HOME}cmd/kwsk-server/main.go --port 8080 --istio $(minikube ip):32000 >kwsk.log 2>&1 &
 KWSK_PID=$!
 
 pushd $OWSK_HOME
-./gradlew :tests:test --tests "system.basic.WskRest*"
+./gradlew :tests:test --tests ${TESTS:-"system.basic.WskRest*"}
 STATUS=$?
 popd
-
-ps -f
 kill -- "-$KWSK_PID"
 
 exit $STATUS
