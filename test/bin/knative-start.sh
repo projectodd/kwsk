90bbfa68d598a2337282dad1b155728d17271e79
Jim Crossley <jim@crossleys.org>
2018-07-05 08:56:20 -0400

    Make start work outside of CI as well

--- test/bin/knative-start.sh
+++ test/bin/knative-start.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+K8S_VERSION=${K8S_VERSION:-v1.10.4}
+VM_DRIVER=${VM_DRIVER:-kvm2}
+BOOTSTRAPPER=${BOOTSTRAPPER:-kubeadm}
+
+if [ "$CI" = "true" ]; then
+  K8S_VERSION=v1.10.0
+  VM_DRIVER=none
+  BOOTSTRAPPER=localkube
+fi
+
 exec minikube start --memory=8192 --cpus=4 \
-  --kubernetes-version=v1.10.0 \
-  --vm-driver=none \
-  --bootstrapper=localkube \
-  --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
-  --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
-  --extra-config=apiserver.admission-control="DenyEscalatingExec,LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+     --kubernetes-version=$K8S_VERSION \
+     --vm-driver=$VM_DRIVER \
+     --bootstrapper=$BOOTSTRAPPER \
+     --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
+     --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
+     --extra-config=apiserver.admission-control="DenyEscalatingExec,LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
