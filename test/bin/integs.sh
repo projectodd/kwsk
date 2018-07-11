2417b1095e16b268c1033bce172833f62c14453b
Jim Crossley <jim@crossleys.org>
2018-07-11 14:25:29 -0400

    Minor cleanup, prevent missing file error

--- test/bin/integs.sh
+++ test/bin/integs.sh
@@ -6,14 +6,14 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TESTDIR="$SCRIPTDIR/.."
 
 OWSK_HOME=$TESTDIR/openwhisk
-KWSK_HOME=$1
 
 if [ ! -d "$OWSK_HOME" ]; then
   git clone https://github.com/apache/incubator-openwhisk.git $OWSK_HOME
-  sed -e "s:OPENWHISK_HOME:$OWSK_HOME:" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
+  cp $TESTDIR/etc/openwhisk-server-cert.pem $OWSK_HOME/ansible/roles/nginx/files/
 fi
+sed -e "s:OPENWHISK_HOME:$OWSK_HOME:" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
 ISTIO=$(minikube ip):$(kubectl get svc knative-ingressgateway -n istio-system -o 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-setsid go run ${KWSK_HOME}cmd/kwsk-server/main.go --port 8080 --istio $ISTIO >kwsk.log 2>&1 &
+setsid go run cmd/kwsk-server/main.go --port 8080 --istio $ISTIO >kwsk.log 2>&1 &
 KWSK_PID=$!
 
 pushd $OWSK_HOME
