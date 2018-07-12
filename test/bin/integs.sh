1b6e2be335b43f4f8f70005b7c043516ea91c0fb
Jim Crossley <jim@crossleys.org>
2018-07-12 08:27:21 -0400

    Use nohup/pkill for cross-platform compat, thx Alan!

--- test/bin/integs.sh
+++ test/bin/integs.sh
@@ -14,15 +14,7 @@ fi
 sed -e "s:OPENWHISK_HOME:$OWSK_HOME:" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
 ISTIO=$(minikube ip):$(kubectl get svc knative-ingressgateway -n istio-system -o 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
 
-OS_NAME=`uname`
-
-if [ "${OS_NAME}" = "Darwin" ]
-then
-  nohup go run ${KWSK_HOME}cmd/kwsk-server/main.go --port 8080 --istio $ISTIO >kwsk.log 2>&1 &
-else
-  setsid go run ${KWSK_HOME}cmd/kwsk-server/main.go --port 8080 --istio $ISTIO >kwsk.log 2>&1 &
-fi
-
+nohup go run $TESTDIR/../cmd/kwsk-server/main.go --port 8080 --istio $ISTIO >kwsk.log 2>&1 &
 KWSK_PID=$!
 
 pushd $OWSK_HOME
@@ -30,11 +22,5 @@ pushd $OWSK_HOME
 STATUS=$?
 popd
 
-if [ "${OS_NAME}" = "Darwin" ]
-then
-  pkill -P "$KWSK_PID"
-else
-  kill -- "-$KWSK_PID"
-fi
-
+pkill -P "$KWSK_PID"
 exit $STATUS
