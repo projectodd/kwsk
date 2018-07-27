1171ea36c4b13a67f779d4e412e38e7b5c0c41a0
Ben Browning <bbrownin@redhat.com>
2018-07-27 14:43:53 -0400

    Put a max timeout on waiting for Istio & Knative to deploy in Travis
    And spit out some cluster info if either fails to come up in time

--- test/bin/knative-install.sh
+++ test/bin/knative-install.sh
@@ -5,21 +5,60 @@ set -x
 curl -L https://storage.googleapis.com/knative-releases/serving/latest/istio.yaml \
   | sed 's/LoadBalancer/NodePort/' \
   | kubectl apply -f -
+
 # label the default namespace with istio-injection=enabled.
 kubectl label namespace default istio-injection=enabled
-# wait until each istio pod is up
-while kubectl get pods -n istio-system | grep -v -E "(Running|Completed|STATUS)"; do
-    sleep 5
+
+TIMEOUT=600 # in seconds
+INTERVAL=5  # in seconds
+
+ELAPSED=0
+PASSED=false
+until [ $ELAPSED -ge $TIMEOUT ]; do
+  kubectl get pods -n istio-system | grep -v -E "(Running|Completed|STATUS)"
+  EXIT_STATUS=$?
+  if [ $EXIT_STATUS -eq 1 ]; then
+    PASSED=true
+    break
+  fi
+
+  let TIMEOUT=TIMEOUT+$INTERVAL
+  sleep $INTERVAL
 done
+if [ "$PASSED" = false ]; then
+  echo "Failed to deploy Istio within $TIMEOUT seconds"
+  kubectl get pods -o wide -n istio-system
+  kubectl describe node
+  kubectl get events
+  exit 1
+fi
+
 
 # install knative
 curl -L https://storage.googleapis.com/knative-releases/serving/latest/release-lite.yaml \
   | sed 's/LoadBalancer/NodePort/' \
   | kubectl apply -f -
-# wait until each knative pod is up
-while kubectl get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do
-    sleep 5
+
+ELAPSED=0
+PASSED=false
+until [ $ELAPSED -ge $TIMEOUT ]; do
+  kubectl get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"
+  EXIT_STATUS=$?
+  if [ $EXIT_STATUS -eq 1 ]; then
+    PASSED=true
+    break
+  fi
+
+  let TIMEOUT=TIMEOUT+$INTERVAL
+  sleep $INTERVAL
 done
+if [ "$PASSED" = false ]; then
+  echo "Failed to deploy Knative within $TIMEOUT seconds"
+  kubectl get pods -o wide -n knative-serving
+  kubectl describe node
+  kubectl get events
+  exit 1
+fi
 
 set +x
 echo "Knative successfully installed!"
