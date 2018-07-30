20aec6c9fb45a3767d2e6048952cb3429666ed7c
Ben Browning <bbrownin@redhat.com>
2018-07-30 14:23:11 -0400

    Point kwsk at the right istio port

--- test/bin/integs.sh
+++ test/bin/integs.sh
@@ -6,7 +6,7 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TESTDIR="$SCRIPTDIR/.."
 
 OWSK_HOME=$TESTDIR/openwhisk
-KWSK_PORT=8181
+KWSK_PORT=8180
 
 if [ ! -d "$OWSK_HOME" ]; then
   git clone -b kwsk-tests --single-branch https://github.com/projectodd/incubator-openwhisk.git $OWSK_HOME
@@ -15,7 +15,7 @@ fi
 sed -e "s:OPENWHISK_HOME:$OWSK_HOME:; s/:8080/:$KWSK_PORT/" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
 
 ISTIO="localhost:8181"
-nohup kubectl port-forward -n istio-system deployment/knative-ingressgateway 8880:80 >portforward.log 2>&1 &
+nohup kubectl port-forward -n istio-system deployment/knative-ingressgateway 8181:80 >portforward.log 2>&1 &
 PORTFORWARD_PID=$!
 
 nohup go run $TESTDIR/../cmd/kwsk-server/main.go --port $KWSK_PORT --istio $ISTIO >kwsk.log 2>&1 &
