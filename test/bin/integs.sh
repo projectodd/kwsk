c330a6d72c9930b27bc4194eb76d8e86b60117d3
Ben Browning <bbrownin@redhat.com>
2018-07-30 14:10:23 -0400

    We can simplify integ ip/port lookup by using `kubectl port-forward`

--- test/bin/integs.sh
+++ test/bin/integs.sh
@@ -6,7 +6,7 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TESTDIR="$SCRIPTDIR/.."
 
 OWSK_HOME=$TESTDIR/openwhisk
-KWSK_PORT=8180
+KWSK_PORT=8181
 
 if [ ! -d "$OWSK_HOME" ]; then
   git clone -b kwsk-tests --single-branch https://github.com/projectodd/incubator-openwhisk.git $OWSK_HOME
@@ -14,14 +14,9 @@ if [ ! -d "$OWSK_HOME" ]; then
 fi
 sed -e "s:OPENWHISK_HOME:$OWSK_HOME:; s/:8080/:$KWSK_PORT/" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
 
-if [ "$(kubectl config current-context)" == "minikube" ]; then
-  NODE_IP=$(minikube ip)
-else
-  NODE_NAME=$(kubectl get node --no-headers | head -n 1 | awk '{print $1}')
-  NODE_IP=$(kubectl get node ${NODE_NAME} -o 'jsonpath={.status.addresses[?(@.type=="InternalIP")].address}')
-fi
-NODE_PORT=$(kubectl get svc knative-ingressgateway -n istio-system -o 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-ISTIO=${NODE_IP}:${NODE_PORT}
+ISTIO="localhost:8181"
+nohup kubectl port-forward -n istio-system deployment/knative-ingressgateway 8880:80 >portforward.log 2>&1 &
+PORTFORWARD_PID=$!
 
 nohup go run $TESTDIR/../cmd/kwsk-server/main.go --port $KWSK_PORT --istio $ISTIO >kwsk.log 2>&1 &
 KWSK_PID=$!
@@ -32,4 +27,6 @@ STATUS=$?
 popd
 
 pkill -P "$KWSK_PID"
+pkill -P "$PORTFORWARD_PID"
+
 exit $STATUS
