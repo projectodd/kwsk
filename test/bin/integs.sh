220b64eea45891a30abfcdcedd0012b4ecbad6d7
Ben Browning <bbrownin@redhat.com>
2018-07-30 11:47:55 -0400

    Port 8080 conflicts with kubeadm-dind

--- test/bin/integs.sh
+++ test/bin/integs.sh
@@ -6,15 +6,16 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TESTDIR="$SCRIPTDIR/.."
 
 OWSK_HOME=$TESTDIR/openwhisk
+KWSK_PORT=8180
 
 if [ ! -d "$OWSK_HOME" ]; then
   git clone -b kwsk-tests --single-branch https://github.com/projectodd/incubator-openwhisk.git $OWSK_HOME
   cp $TESTDIR/etc/openwhisk-server-cert.pem $OWSK_HOME/ansible/roles/nginx/files/
 fi
-sed -e "s:OPENWHISK_HOME:$OWSK_HOME:" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
+sed -e "s:OPENWHISK_HOME:$OWSK_HOME:; s/:8080/:$KWSK_PORT/" <$TESTDIR/etc/whisk.properties >$OWSK_HOME/whisk.properties
 ISTIO=$(minikube ip):$(kubectl get svc knative-ingressgateway -n istio-system -o 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
 
-nohup go run $TESTDIR/../cmd/kwsk-server/main.go --port 8080 --istio $ISTIO >kwsk.log 2>&1 &
+nohup go run $TESTDIR/../cmd/kwsk-server/main.go --port $KWSK_PORT --istio $ISTIO >kwsk.log 2>&1 &
 KWSK_PID=$!
 
 pushd $OWSK_HOME
