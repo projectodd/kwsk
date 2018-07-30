e5a82ba9edc2890204dc087c7b31e6ddfeb700f0
Ben Browning <bbrownin@redhat.com>
2018-07-30 11:19:21 -0400

    use the dind kubectl context

--- test/bin/kubeadm-dind-start.sh
+++ test/bin/kubeadm-dind-start.sh
@@ -14,5 +14,11 @@ export NUM_NODES=1
 # export CONTROLLER_MANAGER_cluster_signing_key_file="/var/lib/localkube/certs/ca.key"
 # export API_SERVER_admission_controller="DenyEscalatingExec,LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 
+# Not sure if this is needed, but why not
 bash -x dind-cluster.sh clean
+
+# Bring Kubernetes up
 bash -x dind-cluster.sh up
+
+# Default to the newly created kubectl context
+kubectl config use-context dind
