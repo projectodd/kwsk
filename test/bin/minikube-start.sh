0c354e8721f32e6b2f757333aa698c8ba8ddf9d2
Ben Browning <bbrownin@redhat.com>
2018-08-13 13:45:20 -0400

    Allow overriding minikube memory and CPU amounts
    Do this by setting the MINIKUBE_MEMORY and MINIKUBE_CPUS environment
    variables.

--- test/bin/minikube-start.sh
+++ test/bin/minikube-start.sh
@@ -3,6 +3,8 @@
 K8S_VERSION=${K8S_VERSION:-v1.10.5}
 VM_DRIVER=${VM_DRIVER:-kvm2}
 BOOTSTRAPPER=${BOOTSTRAPPER:-kubeadm}
+MINIKUBE_MEMORY=${MINIKUBE_MEMORY:-8192}
+MINIKUBE_CPUS=${MINIKUBE_CPUS:-4}
 
 if [ "$CI" = "true" ]; then
   K8S_VERSION=v1.10.0
@@ -10,7 +12,7 @@ if [ "$CI" = "true" ]; then
   BOOTSTRAPPER=localkube
 fi
 
-exec minikube start --memory=8192 --cpus=4 \
+exec minikube start --memory=$MINIKUBE_MEMORY --cpus=$MINIKUBE_CPUS \
      --kubernetes-version=$K8S_VERSION \
      --vm-driver=$VM_DRIVER \
      --bootstrapper=$BOOTSTRAPPER \
